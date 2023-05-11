5b575b498f9add5dc46c241715fe6916c9db10ec
Alice <lamalice9496@gmail.com>
2023-05-10 20:42:45 -0600

    Set up view binding for QuizQuestionActivity

--- app/src/main/java/com/example/countryquiz/QuizQuestionsActivity.kt
+++ app/src/main/java/com/example/countryquiz/QuizQuestionsActivity.kt
@@ -3,14 +3,46 @@ package com.example.countryquiz
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
 import com.example.countryquiz.R.*
+import com.example.countryquiz.databinding.ActivityQuizQuestionsBinding
+import org.w3c.dom.Text
 
 class QuizQuestionsActivity : AppCompatActivity() {
+
+    private var progressBar : ProgressBar? = null
+    private var tvProgressBar : TextView? = null
+    private var tvQuestion: TextView? = null
+    private var tvImage: ImageView? = null
+    private var tvOptionOne: TextView? = null
+    private var tvOptionTwo: TextView? = null
+    private var tvOptionThree: TextView? = null
+    private var tvOptionFour: TextView? = null
+
+    private lateinit var binding: ActivityQuizQuestionsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(layout.activity_quiz_questions)
+        binding = ActivityQuizQuestionsBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
         val questionsList = Constants.getQuestions()
+
+        var currentPosition = 1
+        val question = questionsList[currentPosition-1]
+        binding.idImage.setImageResource(question.image)
+        binding.progressBar.progress = currentPosition
+        binding.tvProgress.text = "$currentPosition/${binding.progressBar?.max}"
+        binding.tvQuestion.text = question.question
+        binding.tvOptionOne.text = question.optionOne
+        binding.tvOptionTwo.text = question.optionTwo
+        binding.tvOptionThree.text = question.optionThree
+        binding.tvOptionFour.text = question.optionFour
+
+
+
 
     }
 }
